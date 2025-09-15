e698fcc3f1bacb9e1b63af0d2f118c210348369f
Simone Mercer <simoneblue12@gmail.com>
2025-09-14 20:40:53 -0700

    Method Overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,32 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static int Add(int a, int b)
         {
-            Console.WriteLine("Hello, World!");
+            return a + b;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isMoney)
+        {
+            var sum = a + b;
+
+            if (isMoney && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isMoney && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return sum.ToString();
+            }
         }
     }
 }
